a243f1f1d537d7269fd4ce56670d3fc9065add3d
Branden <brandenehman45@gmail.com>
2021-06-06 12:54:34 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", (event) => {
+    const element = document.getElementById("text");
+    const textChange = element.innerHTML = "This is really cool!";
+    return event = textChange;
+});
